f0fb3adcc343ff4be2ef7af0d0c3aa62a56bf1c8
Krzysztof Gruca <kgruca21@gmail.com>
2022-09-11 00:34:16 -0700

    formatting strings from apis 2

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ console.log(flights.split('+'));
 // logs ['_Delayed_Departure;fao93766109;txl2133758440;11:25', '_Arrival;bru0943384722;fao93766109;11:45', '_Delayed_Arrival;hel7439299980;fao93766109;12:05', '_Departure;fao93766109;lis2323639855;12:30']
 for (const flight of flights.split('+')) {
   const [type, from, to, time] = flight.split(';');
-  const output = `${type.replaceAll('_', ' ')} from ${from.slice(0, 3).toUpperCase()} to ${to.slice(0, 3).toUpperCase()} (${time.replace(':', 'h')})`;
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll('_', ' ')} from ${from.slice(0, 3).toUpperCase()} to ${to.slice(0, 3).toUpperCase()} (${time.replace(':', 'h')})`;
   console.log(output);
 }
 // pass 1 logs _Delayed_Departure fao93766109 txl2133758440 (11:25)
